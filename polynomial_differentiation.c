20fb99bc60e00a01387c451e203a2769bade834f
BEAFbetween <benfleming1@live.co.uk>
2020-09-30 15:54:57 +0100

    update

--- polynomial_differentiation.c
+++ polynomial_differentiation.c
@@ -4,5 +4,16 @@
 
 int main()
 {
+    int deg=0;
+    
+    printf("What is the degree of the polynomial?\n");
+    scanf("%d",&deg);
+    
+    for (int i=0; i<=deg; i++)
+    {
+        double coef=0;      // Initialising coefficient for degree deg-i
+        printf("Coefficient of x power %d?\n",deg-i);
+        scanf("%lf",&coef);
+    }
     return 0;
 }
